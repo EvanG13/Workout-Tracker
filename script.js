919fcc2532ee0e95dc4133845a2296c839ccb988
Evan Giampaoli <emgiampaoli@gmail.com>
2022-04-10 18:34:28 -0700

    created base file structure and website design

---
+++ script.js
@@ -0,0 +1,9 @@
+class Exercise {
+    constructor(num_sets, num_reps, name, link) {
+        this.num_sets = num_sets;
+        this.num_reps = num_reps;
+        this.name = name;
+        this.link = link;
+
+    }
+}
