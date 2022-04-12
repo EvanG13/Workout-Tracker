e921484b6e9246e0b54094198a9288b6180a901e
Evan Giampaoli <emgiampaoli@gmail.com>
2022-04-11 19:03:47 -0700

    worked on fuctionality for adding an exercise on to exercise circuit

--- script.js
+++ script.js
@@ -1,92 +1,149 @@
 //script for index.html
 // declares exercise class and Exercise circuit class. call the ExerciseCircuit constructor to render the day's exercise
 
-let createButton = document.getElementById("create-workout");
+const createButton = document.getElementById("create-workout");
+const addButton = document.getElementById("add-exercise");
+let list = []                                                        /// exercise list  
+let armDay;
 
-
+/**
+ * Creates workout plan template for day
+ * 
+ * @param {*} e 
+ */
 let createWorkout = (e) => {
-    //test
-    let firstExerciseRow = new Exercise(5, "8-12", "Skull Crushers",
+    armDay = new ExerciseCircuit("Arm Day");
+    
+    return true;
+}
+
+/**
+ * Creates exercise template user will fill out
+ * 
+ * @param {*} k 
+ */
+let addExercise = () => {
+
+    let firstExercise = new Exercise(5, "8-12", "Skull Crushers",
      "https://www.bing.com/videos/search?q=skull+crusher+exercise&&view=detail&mid=B217EFE5B6B1908BC950B217EFE5B6B1908BC950&&FORM=VRDGAR&ru=%2Fvideos%2Fsearch%3Fq%3Dskull%2Bcrusher%2Bexercise%26qpvt%3Dskull%2Bcrusher%2Bexercise%26FORM%3DVDRE",
       "tricep", "Skull crusher tutorial");
     
-    let secondExerciseRow = new Exercise(3, 
+    let secondExercise = new Exercise(3, 
                                     "12",
                                     "tricep pulldown",
                                     "https://www.bing.com/videos/search?q=rope+pulldown&&view=detail&mid=0061B03BECB5783C199E0061B03BECB5783C199E&&FORM=VRDGAR&ru=%2Fvideos%2Fsearch%3Fq%3Drope%2520pulldown%26qs%3Dn%26form%3DQBVDMH%26%3D%2525eManage%2520Your%2520Search%2520History%2525E%26sp%3D-1%26pq%3Drope%2520pulldown%26sc%3D8-13%26sk%3D%26cvid%3D4F401B911C5343DB8DC97707BEA1B904",
                                     "tricep",
                                     "rope pulldown tutorial");
-    let armDay = new ExerciseCircuit("Arm Day", [firstExerciseRow, secondExerciseRow]);
+    armDay.addExercise(firstExercise);
+    armDay.addExercise(secondExercise);
+}
 
-    return true;
+/**
+ * Save, submit and display newly created exercise
+ */
+let saveExercise = () => {
+
 }
 
 createButton.addEventListener("click", createWorkout);
+addButton.addEventListener("click", addExercise);
 
+/**
+ * Gets current date in format : mm/dd/yyyy
+ * 
+ * @returns current date
+ */
 let getDateString = () => {
     let currentDate = new Date();
-    let cDay = currentDate.getDate();
-    let cMonth = currentDate.getMonth() + 1;
-    let cYear = currentDate.getFullYear();
-    return ( cMonth + "/" + cDay + "/" + cYear);
+    let day = currentDate.getDate();
+    let month = currentDate.getMonth() + 1;
+    let year = currentDate.getFullYear();
+    return (month + "/" + day + "/" + year);
 }
-
+/**
+ *  Class for your exercise circuit
+ */
 class ExerciseCircuit{
-    constructor(circuitTitle, exerciseList) {
+    constructor(circuitTitle) {
         this.circuitTitle = circuitTitle;
-        this.exerciseList = exerciseList;
+        this.exerciseList = [];
         this.date = getDateString();
         //render the date
         document.getElementById("date").innerText = this.date;
         document.getElementById("circuit-title").innerText = this.circuitTitle;
-        //render each exercise row 
-        for(let i =0; i< this.exerciseList.length; i++){
-            exerciseList[i].renderExercise();
+    }
+
+    /**
+     * Adds exercise onto exercise list
+     * 
+     * @param {*} Exercise 
+     */
+    addExercise(Exercise) {
+         //render each exercise row
+         if (Exercise != null) {
+            Exercise.renderExercise();
+        } 
+        else {
+            alert("Error, attempted to add null exercise!");
         }
     }
 }
 
-//one exercise that will be rendered as a row inside the ExerciseCircuit
+/**
+ * one exercise that will be rendered as a row inside the ExerciseCircuit
+ * 
+ */
 class Exercise {
-    constructor(num_sets, num_reps, name, link, muscleGroup, linkName ) {
-        this.num_sets = "number of sets: " + num_sets;
-        this.num_reps = "number of reps: " + num_reps;
+    constructor(numSets, numReps, name, link, muscleGroup, linkName ) {
+        this.numSets = "number of sets: ";
+        this.numSets += numSets;
+        this.numReps = "number of reps: " + numReps;
         this.name = name;
         this.link = link;
         this.muscleGroup = muscleGroup;
         this.linkName = linkName;
     }
 
+    /**
+     * create template user uses to add an exercise to list
+     */
+    renderTemplate = () => {
+        
+    }
+
+    /**
+     * render exercise that the user creates
+     */
     renderExercise = () => {
         let exerciseRows = document.getElementById("exercise-rows"); //the container that holds all exercises
         let exercise = document.createElement("div");
         exercise.classList.add("exercise-row"); //each exercise has a class of exercise-row
-        //setup  exercise Link
+        // setup  exercise Link
         let exerciseLink = document.createElement("a");
         exerciseLink.id = "exercise-link";
         exerciseLink.href = this.link;
         exerciseLink.innerText = this.linkName;
         exercise.appendChild(exerciseLink);
 
-        //setup muscle group
+        // setup muscle group
         let muscleGroup = document.createElement("p");
         muscleGroup.id = "muscle-group";
         muscleGroup.innerText = this.muscleGroup;
         exercise.appendChild(muscleGroup);
 
-        //setup numSets
+        // setup numSets
         let numSets = document.createElement("p");
         numSets.id="num-sets";
-        numSets.innerText = this.num_sets;
+        numSets.innerText = this.numSets;
         exercise.appendChild(numSets);
 
-         //setup numReps
+         // setup numReps
          let numReps = document.createElement("p");
          numReps.id="num-reps";
-         numReps.innerText = this.num_reps;
+         numReps.innerText = this.numReps;
          exercise.appendChild(numReps);
 
-         //append the exercise to the exercise container
+        // append the exercise to the exercise container
         exerciseRows.appendChild(exercise); 
     }
 }
